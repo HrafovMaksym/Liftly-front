264b5cdca5a7230a73437c29859949215f33019d
Bohdan <prokudinbogdan50@gmail.com>
2026-02-28 13:54:13 +0100

    feat(home): add workout fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+// const OLD_API = "http://localhost:3000/api";
+
+const fetchData = async (url: any) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
 
 export default function Home() {
+  const [workouts, setWorkouts] = useState<any>([]);
+
+  useEffect(() => {
+    fetchData("http://localhost:4000/api/workouts").then((data) => {
+      console.log("workouts data:", data);
+      setWorkouts(data);
+    });
+  }, []);
+
+  // TODO: add loading state
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
